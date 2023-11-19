eb412094aadcdebaa9d484a00bc72fab3e5c71da
sanbricio <sanbriciorojas11@gmail.com>
2023-11-19 22:55:33 +0100

    [FEATURE] InitialModal form create server

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
+import { InitialModal } from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
     const profile = await initialProfile();
@@ -21,7 +22,7 @@ const SetupPage = async () => {
     }
 
 
-    return <div>Create a Server</div>;
+    return <InitialModal/>;
 }
 
 export default SetupPage;
