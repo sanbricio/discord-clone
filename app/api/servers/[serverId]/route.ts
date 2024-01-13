5f650c816f68cb8b2319ad102ae9252e78aa8a7d
sanbricio <sanbriciorojas11@gmail.com>
2024-01-13 19:55:53 +0100

    [FEATURE] Delete and Leave server modal

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -29,3 +29,25 @@ export async function PATCH(req: Request, { params }: { params: { serverId: stri
         return new NextResponse("Internal Error", { status: 500 })
     }
 }
+
+export async function DELETE({ params }: { params: { serverId: string } }) {
+    try {
+        const profile = await currentProfile();
+        if (!profile) {
+            return new NextResponse("Unauthorized", { status: 401 })
+        }
+
+        const server = await db.server.delete({
+            where: {
+                id: params.serverId,
+                profileId: profile.id
+            }
+        })
+
+        return NextResponse.json(server)
+
+    } catch (error) {
+        console.log("[SERVER_ID_DELETE]", error)
+        return new NextResponse("Internal Error", { status: 500 })
+    }
+}
