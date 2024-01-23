77a5adea2f88362f35ffca134e3115351067ecad
sanbricio <sanbriciorojas11@gmail.com>
2024-01-23 16:56:09 +0100

    [FEATURE] Set up Socket.io

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import { Hash } from "lucide-react";
 import { MobileToogle } from "@/components/mobile-toggle";
 import { UserAvatar } from "@/components/user-avatar";
+import { SocketIndicator } from "../socket-indicator";
 
 interface ChatHeaderProps {
     serverId: string
@@ -32,7 +33,9 @@ export const ChatHeader = ({
             <p className="font-semibold text-md text-black dark:text-white">
                 {name}
             </p>
-
+            <div className="ml-auto flex items-center">
+                <SocketIndicator/>
+            </div>
         </div>
     )
 }
