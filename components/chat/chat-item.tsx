72fa69e43713abda9db733d7a31b3418fef7b080
sanbricio <sanbriciorojas11@gmail.com>
2024-01-25 16:57:13 +0100

    [FEATURE] Delete message

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -15,6 +15,7 @@ import { cn } from "@/lib/utils"
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { useModal } from "@/hooks/use-modal-store"
 
 interface ChatItemProps {
     id: string
@@ -55,7 +56,7 @@ export const ChatItem = ({
 }: ChatItemProps) => {
 
     const [isEditing, setIsEditing] = useState(false)
-    const [isDeleting, setIsDeleting] = useState(false)
+    const { onOpen } = useModal()
 
     useEffect(() => {
         const handleKeyDown = (event: any) => {
@@ -85,8 +86,8 @@ export const ChatItem = ({
                 query: socketQuery
             })
 
-            await axios.patch(url,values);
-            
+            await axios.patch(url, values);
+
             form.reset()
             setIsEditing(false)
         } catch (error) {
@@ -223,6 +224,10 @@ export const ChatItem = ({
 
                     <ActionTooltip label="Delete">
                         <Trash
+                            onClick={() => onOpen("deleteMessage",{
+                                apiUrl: `${socketUrl}/${id}`,
+                                query: socketQuery
+                            })}
                             className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition"
                         />
                     </ActionTooltip>
