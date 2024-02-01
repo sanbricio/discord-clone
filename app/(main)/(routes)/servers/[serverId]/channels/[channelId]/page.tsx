cb3d8db1d16e051469b2cb9951f25c21531b08ee
sanbricio <sanbriciorojas11@gmail.com>
2024-02-01 23:19:00 +0100

    [FEATURE] Creado DirectMessage

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,6 +1,6 @@
 import { ChatHeader } from "@/components/chat/chat-header"
 import { ChatInput } from "@/components/chat/chat-input"
-import { ChatMessages } from "@/components/chat/chat.messages"
+import { ChatMessages } from "@/components/chat/chat-messages"
 import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db"
 import { redirectToSignIn } from "@clerk/nextjs"
